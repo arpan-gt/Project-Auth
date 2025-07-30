013b7ff61fdafaf8df538838feb3e9f9ed5ef3cd
arpan-gt <arpan.main@outlook.com>
2025-07-30 23:08:19 +0530

    user model updated

--- models/User.models.js
+++ models/User.models.js
@@ -1,42 +1,57 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select:false
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
+    verificationTokenExpires: {
+      type: Date,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  isVerified: {
-    type: Boolean,
-    default: false
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user"
-  },
-  resetPasswordToken: {
-    type: String,
-  },
-  verificationToken: {
-    type: String,
-  },
-  resetPasswordExpires: {
-    type: Date,
+  { timestamps: true }
+);
 
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
+  } catch (err) {
+    return next(err);
   }
-}, { timestamps: true });
+});
 
-const User = mongoose.model("User", userSchema)
-
-// mongoose.pre("save")
-
-export  {User};
+const User = mongoose.model("User", userSchema);
+export { User };
