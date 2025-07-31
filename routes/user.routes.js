2bcca50b23030713472728d0d273dca35b5ee397
arpan-gt <arpan.main@outlook.com>
2025-07-31 13:28:28 +0530

    added new routes -profile,logout,forgotPassword,resetPassword

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,22 @@
 import express from "express";
 const userRouter = express.Router();
-import { login, register, verifyUser } from "../controllers/user.controller.js";
+import {
+  forgotPassword,
+  login,
+  logout,
+  profile,
+  register,
+  resetPassword,
+  verifyUser,
+} from "../controllers/user.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 userRouter.post("/register", register);
 userRouter.get("/verify-email/:token", verifyUser);
 userRouter.post("/login", login);
+userRouter.get("/profile", isLoggedIn, profile);
+userRouter.post("/logout", isLoggedIn, logout);
+userRouter.post("/forgotPassword", forgotPassword);
+userRouter.post("/resetPassword/:token", resetPassword);
 
 export { userRouter };
